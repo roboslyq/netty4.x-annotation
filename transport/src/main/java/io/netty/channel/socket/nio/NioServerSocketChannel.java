0539cd997cc6fdd0b137a06361169f5ed8b0cc30
luo.yongqian <luo.yongqian@yuexiu-finance.com>
2020-07-29 14:25:13 +0800

    updated

--- transport/src/main/java/io/netty/channel/socket/nio/NioServerSocketChannel.java
+++ transport/src/main/java/io/netty/channel/socket/nio/NioServerSocketChannel.java
@@ -170,12 +170,12 @@ public class NioServerSocketChannel extends AbstractNioMessageChannel
      */
     @Override
     protected int doReadMessages(List<Object> buf) throws Exception {
-        // 获取当前客户端与服务端的连接： SocketChannel
+        // 获取当前客户端与服务端的连接(Java JDK层面)： SocketChannel
         SocketChannel ch = SocketUtils.accept(javaChannel());
 
         try {
             // 阻塞模式：ServerSocketChannel.accept() 方法监听新进来的连接，当 accept()方法返回的时候,它返回一个包含新进来的连接的 SocketChannel。阻塞模式下, accept()方法会一直阻塞到有新连接到达。
-            //非阻塞模式：accept() 方法会立刻返回，如果还没有新进来的连接,返回的将是null。 因此，需要检查返回的SocketChannel是否是null。
+            // 非阻塞模式：accept() 方法会立刻返回，如果还没有新进来的连接,返回的将是null。 因此，需要检查返回的SocketChannel是否是null。
             if (ch != null) {
                 // 1、使用Netty的NioSocketChannel包装 JDK 原生的SocketChannel。从而实现Netty与JDK 对接。
                 // 2、在父类的构造函数中，完成当前Channel的pipeline 创建
